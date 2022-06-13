ea050f0e30ae58c1b6f33747c2c4f55ab273f939
Anton Nguyen <afnguyen85@gmail.com>
2022-06-13 13:44:24 -0400

    Allow version overrides

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -94,6 +94,66 @@ describe('Run', () => {
     expect(setFailed).not.toHaveBeenCalled()
   })
 
+  it('allows the version to be overriden', async () => {
+    const versionOverride = 'v1.0.1-beta.1'
+    // how do we mock `core.getInput` to return `versionOverride` if `default-branch` is the input?
+
+    const expectedChangelog = fs.readFileSync(
+      __dirname + '/expected-changelog.md',
+      'utf8'
+    )
+
+    nock('https://api.github.com')
+      .persist()
+      .get('/repos/foo/bar/releases')
+      .reply(200, successfulTagResponse)
+    nock('https://api.github.com')
+      .persist()
+      .get('/repos/foo/bar/compare/v1.0.0...master')
+      .reply(200, successfulComparisonResponse)
+    nock('https://api.github.com')
+      .persist()
+      .post('/repos/foo/bar/releases', {
+        tag_name: versionOverride,
+        name: versionOverride,
+        body: expectedChangelog
+      })
+      .reply(201, successfulComparisonResponse)
+
+    const setOutput = jest.spyOn(core, 'setOutput')
+    const infoMock = jest.spyOn(core, 'info')
+    const setFailed = jest.spyOn(core, 'setFailed')
+    await run()
+
+    expect(infoMock).toHaveBeenCalledWith('Listing releases for foo/bar')
+    expect(infoMock).toHaveBeenCalledWith(
+      'Comparing commits for foo/bar on v1.0.0 against master'
+    )
+    expect(infoMock).toHaveBeenCalledWith('master is behind by 4 commit(s)')
+    expect(infoMock).toHaveBeenCalledWith(
+      'latest release date is 2013-02-27T19:35:32Z'
+    )
+    expect(infoMock).toHaveBeenCalledWith(
+      `Creating release ${versionOverride} for foo/bar`
+    )
+    expect(setOutput).toHaveBeenCalledWith('commit-count', '4')
+    expect(setOutput).toHaveBeenCalledWith('new-version', versionOverride)
+    expect(setOutput).toHaveBeenCalledWith(
+      'release-url',
+      `https://github.com/foo/bar/releases/tag/${versionOverride}`
+    )
+    expect(setOutput).toHaveBeenCalledWith(
+      'diff-url',
+      `https://github.com/foo/bar/compare/v1.0.0...${versionOverride}`
+    )
+    expect(setOutput).toHaveBeenCalledWith(
+      'latest-release-date',
+      '2013-02-27T19:35:32Z'
+    )
+    expect(setOutput).toHaveBeenCalledWith('changelog', expectedChangelog)
+    expect(setFailed).not.toHaveBeenCalled()
+  })
+
   it('does not create a release if the github token env variable is not set', async () => {
     process.env.GITHUB_TOKEN = ''
     const errorMock = jest.spyOn(core, 'error')
